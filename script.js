74019ea0551317dfa190007388d9677e36512013
jameswhitney <33575806+jameswhitney@users.noreply.github.com>
2020-12-03 14:05:50 -0500

    Adds new favicon

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function newQuote() {
   if (!quote.author) {
     authorText.textContent = "Unknown";
   } else {
-    authorText.textContent = quote.author;
+    authorText.textContent = `${quote.author}`;
   }
   // Check Quote length to determine styling
   if (quote.text.length > 120) {
